74cf12ffe268ba9b970ecb1b4da54e9793f481e3
valeriia-z <valeriia.zinoveva@proton.me>
2023-03-15 20:32:58 +0100

    Added visualization to explore correlation between charges and age (+by sex)

--- scripts/hypotesting.R
+++ scripts/hypotesting.R
@@ -1,10 +1,23 @@
 library(tidyverse)
 data <- read.csv("data/insurance.csv")#import data
+
+charges_vs_age_plot <- ggplot(data, aes(age, charges)) + # look if there correlation between charges and age
+  geom_point() +
+  labs(x = "Age", y = "Charges") + 
+  ggtitle("Charges vs. Age")
+
+charges_vs_age_sex_plot <- ggplot(data, aes(age, charges)) + # is sex makes difference?
+  geom_point() +
+  labs(x = "Age", y = "Charges") +
+  ggtitle("Charges vs. Age by Sex") +
+  scale_color_manual(values = c("red", "blue")) +  
+  aes(color = sex)
+
 data_c <- data %>% 
   filter(sex == "female") %>% 
   mutate(Group = ifelse(age >= 35, "Above 35", "Below 35")) #create a new data fr
-                                                            #with females filtered by age
-                                                            #in the new column
+#with females filtered by age
+#in the new column
 data_c %>% 
   count(Group) #women in each group
 
@@ -16,4 +29,4 @@ fig<- data_c %>%
 ggsave(path = "figs", filename = "fig1.png") #save in the figs folder
 
 t.test(charges ~ Group, data = data_c, alternative = "greater")#t-test(check if 
-                                                      #we reject H0 hypothesis)
+#we reject H0 hypothesis)
